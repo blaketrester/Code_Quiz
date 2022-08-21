ce2ef2c9692fb9af76b34dd2998f8ff0cde58a88
Blake Trester <blaketrester@yahoo.com>
2022-08-20 20:19:10 -0600

    added an array containing questions and answers for quiz

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+// Array containing questions and options for answers
+var questions = [
+    {
+      questionText: "What is the largest reptile in the world?",
+      choices: [
+        "1. Komodo Dragon", 
+        "2. Saltwater Crocodile", 
+        "3. Green Anaconda", 
+        "4. Leatherback Sea Turtle"],
+
+      answer: "2. Saltwater Crocodile",
+    },
+
+    {
+      questionText: "The _____ is a species of chameleon that does not lay eggs.",
+      choices: [
+        "1. Jackson's Chameleon",
+        "2. Panther Chameleon",
+        "3. Veiled Chameleon",
+        "4. Flap-necked Chameleon"],
+
+      answer: "1. Jackson's Chameleon",
+    },
+
+    {
+      questionText:
+        "Which of the following lizards are venomous:",
+      choices: [
+        "1. Horned Lizard", 
+        "2. Gila Monster", 
+        "3. Giant Day Gecko", 
+        "4. Frilled Neck Lizard"],
+
+      answer: "2. Gila Monster",
+    },
+
+    {
+      questionText:
+        "Which of the following snakes are non-venomous:",
+      choices: [
+        "1. Coral Snake",
+        "2. Diamondback Rattlesnake",
+        "3. King Cobra",
+        "4. Emerald Tree Boa"],
+
+      answer: "4. Emerald Tree Boa",
+    },
+
+    {
+      questionText:
+        "What is the oldest know reptile?",
+      choices: [
+        "1. Alligator Snapping Turtle", 
+        "2. Common Caimon", 
+        "3. Giant Tortoise", 
+        "4. Green Iguana"],
+
+      answer: "3. Giant Tortoise",
+    },
+    
+  ];
+  
+  
